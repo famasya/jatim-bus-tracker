fdbc283fb5a674082d4d442847df28900873f5bc
Abid Famasya <abid.famasya@gmail.com>
2023-03-01 13:27:08 +0700

    feat: remove transj (hotfix)

--- app/components/transj-bus.tsx
+++ app/components/transj-bus.tsx
@@ -2,7 +2,6 @@ import { Map, Marker as LMarker } from "leaflet";
 import { RefObject, useEffect, useRef, useState } from "react";
 import { Marker, Polyline, Popup } from "react-leaflet";
 import ReactLeafletDriftMarker from "react-leaflet-drift-marker";
-import { io, Socket } from "socket.io-client";
 import stc from "string-to-color";
 import { useFilterState } from "~/common/states";
 import { BusIcon } from "~/icons/bus";
@@ -41,24 +40,24 @@ export default function TransJBusPos(props: { mapRef: RefObject<Map> }) {
     busStops.map((stop) =>
       storeBusStops("Trans Jatim", stop.kor, stop.sh_name)
     );
-    loadTransJPositions()
-      .then((results) => {
-        setBusPositions(results);
-      })
-      .then(() => {
-        const socket: Socket = io(`${window.ENV.TRANSJ_TRACKER_ENDPOINT}`);
-        socket.on("update_jatim", (event: TransJEvent) => {
-          setBusPositions((positions) =>
-            positions.map((item, i) => {
-              if (item.id === event.id) {
-                item["lat"] = event.lat;
-                item["lng"] = event.lng;
-              }
-              return item;
-            })
-          );
-        });
-      });
+    // loadTransJPositions()
+    //   .then((results) => {
+    //     setBusPositions(results);
+    //   })
+    //   .then(() => {
+    //     const socket: Socket = io(`${window.ENV.TRANSJ_TRACKER_ENDPOINT}`);
+    //     socket.on("update_jatim", (event: TransJEvent) => {
+    //       setBusPositions((positions) =>
+    //         positions.map((item, i) => {
+    //           if (item.id === event.id) {
+    //             item["lat"] = event.lat;
+    //             item["lng"] = event.lng;
+    //           }
+    //           return item;
+    //         })
+    //       );
+    //     });
+    //   });
 
     if (selectedBusStop) {
       const marker = markerRef.current;
